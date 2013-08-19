24115796e6072a4369dc911e46dc4618227801b6
Tomaz Nedeljko <tomaz.nedeljko@gmail.com>
2013-08-19 14:02:14 +0200

    Add TOMTextView

---
+++ TOMKit/TOMTextView.h
@@ -0,0 +1,51 @@
+//
+//  TOMTextView.h
+//  TOMKit
+//
+//  Copyright (c) 2013 Tomaz Nedeljko (http://nedeljko.com)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@class TOMTextView;
+
+@protocol TOMTextViewDelegate <UITextViewDelegate>
+
+@optional
+
+- (CGSize)minimumAutogrowSizeForTextView:(TOMTextView *)textView;
+- (void)textView:(TOMTextView *)textView willAutogrowToSize:(CGSize)size;
+- (void)textView:(TOMTextView *)textView didAutogrowToSize:(CGSize)size;
+
+@end
+
+@interface TOMTextView : UITextView
+
+@property (weak, nonatomic) IBOutlet id<TOMTextViewDelegate> delegate;
+@property (copy, nonatomic) NSString *placeholder;
+@property (strong, nonatomic) UIFont *placeholderFont;
+@property (strong, nonatomic) UIColor *placeholderColor;
+@property (nonatomic) BOOL autogrow;
+
+- (void)resizeToFit;
+
+@end
